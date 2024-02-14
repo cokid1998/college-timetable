477fbb100a415855b8514ed4d82450d7ab4630b6
cokid1998 <cokid98@naver.com>
2024-02-14 10:52:35 +0900

    default timetable layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Calendar from "@toast-ui/react-calendar";
+import "@toast-ui/calendar/dist/toastui-calendar.min.css";
+
+const initialEvents = [
+  {
+    id: "1",
+    calendarId: "cal1",
+    title: "Lunch",
+    category: "time",
+    body: "TOAST UI Calendar",
+    start: "2024-02-14T10:00:00",
+    end: "2024-02-14T12:30:00",
+    backgroundColor: "#03bd9e",
+    location: "Meeting Room A",
+  },
+  {
+    id: "2",
+    calendarId: "cal1",
+    title: "Coffee Break",
+    category: "time",
+    start: "2022-06-28T15:00:00",
+    end: "2022-06-28T15:30:00",
+  },
+];
+
+const calendarOptions = {
+  isReadOnly: true,
+  events: initialEvents,
+  week: {
+    startDayOfWeek: 1,
+    dayNames: ["일", "월", "화", "수", "목", "금", "토"],
+    workweek: true,
+    hourStart: 8,
+    hourEnd: 20,
+    taskView: false,
+    eventView: ["time"],
+  },
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Calendar
+        {...calendarOptions}
+        height="100svh"
+        theme={{
+          week: {
+            dayGrid: {},
+            dayGridLeft: {
+              width: "45px",
+            },
+            timeGridLeft: {
+              width: "40px",
+            },
+            timeGridHalfHourLine: {
+              borderBottom: "1px dotted #e5e5e5",
+            },
+            nowIndicatorLabel: {
+              color: "#515DE6",
+            },
+            nowIndicatorBullet: {
+              backgroundColor: "#515ce6",
+            },
+          },
+        }}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
