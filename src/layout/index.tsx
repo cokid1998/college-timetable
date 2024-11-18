32a44f940d436d18440fc9e1e044769404a5c553
cokid1998 <cokid98@naver.com>
2024-11-18 17:45:33 +0900

    Add max-width in layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,10 +2,10 @@ import { PropsWithChildren } from "react";
 import NextClassTimer from "../components/\bNextClassTimer";
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <>
+    <div style={{ maxWidth: "767px", margin: "0 auto" }}>
       <NextClassTimer />
       {children}
-    </>
+    </div>
   );
 };
 
